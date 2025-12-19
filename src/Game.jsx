475df89bca1c581d59592ddb47604c3f0c647189
Arpit Agarwa1 <arpitagrwal932@gmail.com>
2025-12-19 18:39:39 +0530

    upodating

--- src/Game.jsx
+++ src/Game.jsx
@@ -6,7 +6,10 @@ export default function Game() {
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const [time, setTime] = useState(10);
 
+  const [screen, setScreen] = useState(1);
+
   useEffect(() => {
+    if (screen !== 2) return;
     const interval = setInterval(handelTimer, 1000);
 
     function handelTimer() {
@@ -14,14 +17,22 @@ export default function Game() {
 
       setTime((prev) => {
         if (prev === 0) {
+          setScreen(3);
           clearInterval(interval);
-          alert("Game Over!");
+          //   alert("Game Over!");
           return 0;
         }
         return prev - 1;
       });
     }
-  }, []);
+  }, [screen]);
+
+  function startGame() {
+    setScore(0);
+    setTime(10);
+    setScreen(2);
+    handleRandomPosition();
+  }
 
   function handleClick() {
     setScore((prev) => prev + 1);
@@ -38,20 +49,27 @@ export default function Game() {
   }
 
   return (
-    <div>
-      <h2 className="score">Score: {score}</h2>
-      <h1>{time}</h1>
+    <>
+      {screen === 1 && <button onClick={startGame}> Start Game</button>}
 
-      <div className="box">
-        <div
-          className="square"
-          onClick={handleClick}
-          style={{
-            top: position.top + "px",
-            left: position.left + "px",
-          }}
-        ></div>
-      </div>
-    </div>
+      {screen === 2 && (
+        <div>
+          <h2 className="score">Score: {score}</h2>
+          <h1>{time}</h1>
+
+          <div className="box">
+            <div
+              className="square"
+              onClick={handleClick}
+              style={{
+                top: position.top + "px",
+                left: position.left + "px",
+              }}
+            ></div>
+          </div>
+        </div>
+      )}
+      {screen === 3 && <h1>Game Over! Your final score is {score}</h1>}
+    </>
   );
 }
